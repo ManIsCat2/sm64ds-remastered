1abba163ef6b70f30458f1e4e08bd790db262cde
ExcellentGamer <75348481+ExcellentGamer@users.noreply.github.com>
2024-11-11 02:30:50 -0500

    so it works if you star there ig

--- levels/si/script.c
+++ levels/si/script.c
@@ -11,6 +11,7 @@
 #include "levels/scripts.h"
 
 #include "actors/common1.h"
+#include "actors/group11.h"
 
 #include "make_const_nonconst.h"
 #include "levels/si/header.h"
@@ -19,10 +20,7 @@ const LevelScript level_si_entry[] = {
     INIT_LEVEL(),
     LOAD_MIO0(0x07, _si_segment_7SegmentRomStart, _si_segment_7SegmentRomEnd), 
 	LOAD_MIO0(0x0A, _water_skybox_mio0SegmentRomStart, _water_skybox_mio0SegmentRomEnd), 
-    LOAD_MIO0(0x05, _group4_mio0SegmentRomStart, _group4_mio0SegmentRomEnd),
-    LOAD_RAW(0x0C, _group4_geoSegmentRomStart,  _group4_geoSegmentRomEnd),
-    LOAD_MIO0(0x06, _group13_mio0SegmentRomStart, _group13_mio0SegmentRomEnd),
-    LOAD_RAW(0x0D, _group13_geoSegmentRomStart,  _group13_geoSegmentRomEnd),
+    LOAD_MODEL_FROM_GEO(MODEL_ENEMY_LAKITU, enemy_lakitu_geo),
     ALLOC_LEVEL_POOL(),
     MARIO(MODEL_MARIO, BPARAM4(0x01), bhvPlayer),
     JUMP_LINK(script_func_global_5),
@@ -33,6 +31,16 @@ const LevelScript level_si_entry[] = {
         WARP_NODE(WARP_NODE_SUCCESS, LEVEL_CASTLE, 1, WARP_NODE_0A, WARP_NO_CHECKPOINT),
         WARP_NODE(WARP_NODE_DEATH, LEVEL_CASTLE, 1, WARP_NODE_0A, WARP_NO_CHECKPOINT),
         OBJECT(MODEL_NONE, 3277, -946, 819, 0, -90, 0, BPARAM2(0x0A), bhvSpinAirborneWarp),
+        OBJECT(MODEL_SILVER_STAR, -870, -946, -2355, 0, 0, 0, 0x00000000, bhvSilverStar),
+		OBJECT(MODEL_SILVER_STAR, -2765, -639, 2150, 0, 0, 0, 0x00000000, bhvSilverStar),
+		OBJECT(MODEL_SILVER_STAR, -2519, 334, -2898, 0, 0, 0, 0x00000000, bhvSilverStar),
+		OBJECT(MODEL_SILVER_STAR, -641, 181, -1999, 0, 0, 0, 0x00000000, bhvSilverStar),
+		OBJECT(MODEL_NONE, 3626, -690, 206, 0, 0, 0, BPARAM1(STAR_INDEX_ACT_1), bhvStarSwitchSpawnCondition),
+		OBJECT(MODEL_NONE, -847, 93, -2221, 0, 0, 0, 0x00000000, bhvPoleGrabbing),
+		OBJECT(MODEL_NONE, -2519, -628, -2898, 0, 0, 0, 0x00000000, bhvPoleGrabbing),
+		OBJECT(MODEL_NONE, 246, -946, 3502, 0, 0, 0, 0x00000000, bhvPoleGrabbing),
+		OBJECT(MODEL_NONE, 3649, -946, 632, 0, 0, 0, 0x00000000, bhvPoleGrabbing),
+		OBJECT(MODEL_NONE, 3564, -946, -287, 0, 0, 0, 0x00000000, bhvPoleGrabbing),
         TERRAIN(si_area_1_collision),
         MACRO_OBJECTS(si_area_1_macro_objs),
         SET_BACKGROUND_MUSIC(0, SEQ_SOUND_PLAYER),
