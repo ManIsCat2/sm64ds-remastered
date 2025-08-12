d2c1734d17b33c8a66b9bbdba5b6cc05e50fd4bf
ExcellentGamer <excellentgamer417@gmail.com>
2025-08-12 03:56:34 -0400

    king bobomb model... added, crashes on n64

--- actors/king_bobomb/anims/table.inc.c
+++ actors/king_bobomb/anims/table.inc.c
@@ -1,12 +1,12 @@
 // 0x0500FE30
 const struct Animation *const king_bobomb_seg5_anims_0500FE30[] = {
-    &king_bobomb_seg5_anim_0500BDFC,
-    &king_bobomb_seg5_anim_0500C2AC,
-    &king_bobomb_seg5_anim_0500C520,
-    &king_bobomb_seg5_anim_0500C774,
-    &king_bobomb_seg5_anim_0500CFCC,
-    &king_bobomb_seg5_anim_0500D5B0,
-    &king_bobomb_seg5_anim_0500D978,
+    &king_bobomb_seg5_anim_0500BDFC, // grab animation
+    &king_bobomb_seg5_anim_0500C2AC, // walk while holding
+    &king_bobomb_seg5_anim_0500C520, // thrown?
+    &king_bobomb_seg5_anim_0500C774, // ???
+    &king_bobomb_seg5_anim_0500CFCC, // start
+    &king_bobomb_seg5_anim_0500D5B0, // wait
+    &king_bobomb_seg5_anim_0500D978, // jump
     &king_bobomb_seg5_anim_0500DDD8,
     &king_bobomb_seg5_anim_0500E10C,
     &king_bobomb_seg5_anim_0500F078,
